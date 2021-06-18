b053b446cd55dc655bc9675e73d885769e884314
sophie-512 <70555100+sophie-512@users.noreply.github.com>
2021-06-18 21:22:34 +0900

    Add files via upload

--- pr.js
+++ pr.js
@@ -1,17 +1,15 @@
-const time=document.getElementById('time');
-const header-text=document.getElementByClassName("header-text");
-const header-text1=document.getElementByClassName("header-text1");
+var time=document.getElementById('time');
 
 let change=true;
 
-time.addEventListener('click',function(){
+time.addEventListener('click', function(){
     change=!change;
     if(change){
-        header-text.style.display='block';
-        header-text1.style.display='none';
+        document.getElementById("header-text").style.display = "block";
+        document.getElementById("header-text1").style.display = "none";
     }else{
-        header-text.style.display='none';
-        header-text1.style.display='block';
+        document.getElementById("header-text").style.display = "none";
+        document.getElementById("header-text1").style.display = "block";
     }
 })
 
